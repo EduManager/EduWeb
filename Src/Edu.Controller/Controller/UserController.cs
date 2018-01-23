e34386f7aeb7e59e2fc8536a706a0d3566389142
jerry <alick_wjj@163.com>
2018-01-23 19:07:45 +0800

    添加登陆页面

---
+++ Src/Edu.Controller/Controller/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Edu.Controller.Controller
+{
+    public class UserController : System.Web.Mvc.Controller
+    {
+        public ViewResult Login()
+        {
+            return View();
+        }
+    }
+}
