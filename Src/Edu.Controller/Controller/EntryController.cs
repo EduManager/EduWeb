5d7712efab196d2828709a71ea44778a4ed8f7b1
lird666 <lird_bj@sina.com>
2018-05-07 19:00:19 +0800

    报名

---
+++ Src/Edu.Controller/Controller/EntryController.cs
@@ -0,0 +1,28 @@
+﻿using Edu.Controller.Common;
+using Edu.Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Edu.Controller.Controller
+{
+    [AuthFilter]
+    public class EntryController : System.Web.Mvc.Controller
+    {
+        //public ViewResult List()
+        //{
+        //    var schoolId = ApplicationContext.SchoolId;
+        //    var result = BookfeeService.Instance.GetFeeBySchoolId(new GetObjectByIdArgs()
+        //    {
+        //        SchoolId = schoolId
+        //    });
+        //    var fees = new List<Bookfee>();
+        //    if (result.Code == 200)
+        //        fees = result.Items;
+        //    return View(fees);
+        //}
+    }
+}
